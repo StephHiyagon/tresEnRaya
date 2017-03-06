8ef798aed40bf468ce5f20d5bfeeccdca92b9f42
StephHiyagon <mv.hiyagon@gmail.com>
2017-03-05 21:05:47 -0500

    centrado en css y boton new game

--- js/app.js
+++ js/app.js
@@ -28,20 +28,20 @@ var posicionAMarcar= idCelda[1]-1;
 console.log(posicionAMarcar);
 
 queTurno = turno%2;
-if(queTurno != 0){
+if(queTurno == 0){
   celda.innerHTML = "X";
   celda.style.background = "#EC673A";
   arregloGato[posicionAMarcar] = "X";
   ganaJugador("X");
 }
-else if (queTurno == 0) {
+else {
   celda.innerHTML = "O";
   celda.style.background = "#1C5F81";
   arregloGato[posicionAMarcar] = "O";
   ganaJugador("O");
 }
 console.log(turno,queTurno,arregloGato);
-if(turno == 9){
+if(turno >= 9){
   alert("empate!!!");
 //  window.location.reload();
 }
@@ -61,3 +61,83 @@ function cargarDocument(){
 }
 
 window.addEventListener("load",cargarDocument);
+
+function recargar (){
+  window.location.reload();
+}
+/*
+var turno = 1;
+var queTurno;
+var arregloGato = new Array(9);
+var celdas = document.getElementsByClassName("gato");
+//var celdas = document.getElementsByTagName("td");
+// declaracion de funciones
+function ganaJugador(letra){
+if(
+	(arregloGato[0]==letra && arregloGato[1]==letra && arregloGato[2]==letra)
+	||
+	(arregloGato[3]==letra && arregloGato[4]==letra && arregloGato[5]==letra)
+	||
+	(arregloGato[6]==letra && arregloGato[7]==letra && arregloGato[8]==letra)
+	||
+	(arregloGato[0]==letra && arregloGato[3]==letra && arregloGato[6]==letra)
+	||
+	(arregloGato[1]==letra && arregloGato[4]==letra && arregloGato[7]==letra)
+	||
+	(arregloGato[2]==letra && arregloGato[5]==letra && arregloGato[8]==letra)
+	||
+	(arregloGato[0]==letra && arregloGato[4]==letra && arregloGato[8]==letra)
+	||
+	(arregloGato[2]==letra && arregloGato[4]==letra && arregloGato[6]==letra)
+  ){
+	alert("jugador "+letra+" Gana ");
+}else{
+
+}
+
+}
+
+function gato(evento){
+//se obtiene el id del documento html que se presiono
+// se usa para poner el tache o circulo
+var celda = evento.target;
+celda.removeEventListener("click",gato);
+var idCelda = evento.target.id;
+//alert(idCelda);
+//alert(idCelda.length);
+//alert(idCelda[1]);
+var posicionAMarcar = idCelda[1]-1;
+//alert(posicionAMarcar);
+
+	queTurno = turno%2;
+	if(queTurno!=0)
+	{
+		celda.innerHTML = "X";
+		celda.style.background = "#EC673A";
+		arregloGato[posicionAMarcar] = "X";
+		ganaJugador("X");
+	}else if(queTurno==0){
+		celda.innerHTML = "O";
+		celda.style.background = "#1C5F81";
+		arregloGato[posicionAMarcar] = "O";
+		ganaJugador("O");
+	}
+	console.log(turno,queTurno,arregloGato);
+	if(turno>9){
+		alert("empate");
+	}else{
+		turno++;
+	}
+}
+
+function cargarDocumento(){
+//document.getElementById("C1").addEventListener("click",gato);;
+//document.getElementByClassName("gato")[4].addEventListener("click",gato);
+	n=0;
+	while(n<celdas.length){
+	celdas[n].addEventListener("click",gato);
+	n++;
+	}
+}
+//asignaion de eventos
+window.addEventListener("load",cargarDocumento);*/
